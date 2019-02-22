27b8f44c1572ea3e3f0e6fb2e586c476f960f503
Sergio <s.almendrote@gmail.com>
2019-02-22 23:33:19 +0100

    Ejercicio 1  Tema  4

--- src/Controller/DeportesController.php
+++ src/Controller/DeportesController.php
@@ -2,17 +2,112 @@
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-class DeportesController {
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+class DeportesController extends Controller {
+    // ...
+    /**
+     * @Route("/deportes/usuario", name="usuario" )
+     */
+    public function sesionUsuario(Request $request) {
+        $usuario_get=$request->query->get('nombre');
+        $session = $request->getSession();
+        $session->set('nombre', $usuario_get);
+        return $this->redirectToRoute('usuario_session',array('nombre'=>$usuario_get));
+    }
+    // …
+    /**
+     * @Route("/deportes/usuario/{nombre}", name="usuario_session" )
+     */
+    public function paginaUsuario() {
+        $session=new Session();
+        $usuario=$session->get('nombre');
+        return new Response(sprintf('Sesión iniciada con el atributo nombre: %s', $usuario
+        ));
+    }
     /**
      * @Route("/deportes")
+
      */
     public function inicio() {
         return new Response('Mi página de deportes!');
     }
     /**
-     * @Route("/deportes/primer-articulo")
+     * @Route("/deportes/{slug}")
      */
-    public function mostrar() {
-        return new Response('Mi primera ruta Primer Artículo!');
+    public function mostrar($slug) {
+        return new Response(sprintf(
+            'Mi artículo en mi pagina de deportes: ruta %s',
+            $slug));
     }
+    /**
+     * @Route("/deportes/{seccion}/{pagina}", name="lista_paginas",
+     *      requirements={"page"="\d+"},
+     *      defaults={"section":"tenis"})
+     */
+    public function lista($seccion) {
+        // Simulamos una base de datos de deportes
+        $sports=["futbol", "tenis","rugby"];
+        // Si el deporte que buscamos no se encuentra lanzamos la
+        // excepcion 404 deporte no encontrado
+        if(!in_array($section,$sports)) {
+            throw $this->createNotFoundException('Error 404 este deporte no está en nuestra Base de Datos');
+        }
+        return new Response(sprintf( 'Deportes seccion: seccion %s, listado de noticias página %s', $section, $page));
+    }
+    /**
+     * @Route("/deportes/{seccion}/{slug} ",
+     * defaults={"seccion":"tenis"})
+     */
+    public function noticia($slug, $seccion) {
+
+        return new Response(sprintf(
+            'Noticia de %s, con url dinámica=%s',
+            $seccion, $slug));
+    }
+    /**
+     * @Route(
+     *     "/deportes/{_locale}/{fecha}/{seccion}/{equipo}/{pagina}",
+     *     defaults={"slug": "1","_format":"html","pagina":"1"},
+     *     requirements={
+     *         "_locale": "es|en",
+     *         "_format": "html|json|xml",
+     *         "fecha": "[\d+]{8}",
+     *         "pagina"="\d+"
+     *     }
+     * )
+     */
+    public function rutaAvanzadaListado($_locale, $fecha, $seccion, $equipo, $pagina) {
+        return new Response(sprintf(
+            'Listado de noticias  en idioma=%s,
+      fecha=%s,deporte=%s,equipo=%s, página=%s ',
+            $_locale, $fecha, $seccion, $equipo, $pagina));
+    }
+    /**
+     * @Route(
+     *     "/deportes/{_locale}/{fecha}/{seccion}/{team}/{slug}.{_format}",
+     *     defaults={"slug": "1","_format":"html"},
+     *     requirements={
+     *         "_locale": "es|en",
+     *         "_format": "html|json|xml",
+     *          "date": "[\d+]{8}"
+     *     }
+     * )
+     */
+    public function rutaAvanzada($_locale, $fecha, $seccion, $equipo, $slug) {
+        // Simulamos una base de datos de equipos o personas
+        $sports=["valencia", "barcelona","federer", "rafa-nadal"];
+
+        // Si el equipo o persona que buscamos no se encuentra redirigimos
+        // al usuario a la página de inicio
+        if(!in_array($team,$sports)) {
+            return $this->redirectToRoute('lista_paginas');
+        }
+        return new Response(sprintf(
+            'Mi noticia en idioma=%s,
+      fecha=%s,deporte=%s,equipo=%s, noticia=%s ',
+            $_locale, $fecha, $seccion, $equipo, $pagina));
+    }
+
 }
---
+++ src/EventListener/ExceptionListener.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+class ExceptionListener
+{
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+    // Obtienes el objeto exception del evento recibido
+    $exception = $event->getException();
+    $message = sprintf(
+    'Mi error dice: %s en el código: %s',
+    $exception->getMessage(),
+    $exception->getCode()
+    );
+
+    // Modifica tu response object para mostrar los detalles de excepción
+    $response = new Response();
+    $response->setContent($message);
+
+    // HttpExceptionInterface es un tipo especial de excepción
+    // que mantiene los detalles de header y código de estado
+    if($exception instanceof HttpExceptionInterface) {
+    $response->setStatusCode($exception->getStatusCode());
+    $response->headers->replace($exception->getHeaders());
+    } else {
+    $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
+
+    // Enviar el objeto Response modificado al evento
+    $event->setResponse($response);
+    }
+}
